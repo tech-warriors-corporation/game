ee684ebae4d99495d9872072ddf25d1aca39362a
hotequil <joaohotequil@gmail.com>
2021-09-02 20:05:32 -0300

    Ajustado blink na tela e retirado seleção de imgs.

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,28 @@
 const config = {
     _timeTransitionWindow: 250,
-    init: function(first){
-        if(first){
-            home.show('Em busca da coroa', 'Jogar', './assets/images/home-arthur-and-merlin.png');
-
-            return;
-        }
-
+    element: document.querySelector('[data-game]'),
+    hideClass: 'game--init',
+    init: function(){
         this.step = 1;
         this.lost = false;
 
         config.next();
     },
     next: function(){
+        setTimeout(() => this.element.classList.remove(this.hideClass), this._timeTransitionWindow * 2);
+
         switch(this.step){
             case 1:
                 life.reset();
                 progress.reset();
+                home.show('Em busca da coroa', 'Jogar', './assets/images/home-arthur-and-merlin.png');
                 cutscene.show(
-                    'dasdsaasdadsadsdas',
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     {
                         background: './assets/images/home-arthur.png',
                         merlin: './assets/images/cutscene-merlin-three.png'
                     }
-                )
+                );
                 break;
         }
     },
