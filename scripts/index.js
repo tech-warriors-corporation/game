848d3a23aba9832f87489e6effcd462323be2787
hotequil <joaohotequil@gmail.com>
2021-09-02 21:00:44 -0300

    Iniciada fase da ponte.

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,12 @@ const config = {
     _timeTransitionWindow: 250,
     element: document.querySelector('[data-game]'),
     hideClass: 'game--init',
+    changeBackgroundImage: function(url){
+        this.element.style.backgroundImage = `url(${url})`;
+    },
     init: function(){
+        this.element.classList.add(this.hideClass);
+
         this.step = 1;
         this.lost = false;
 
@@ -15,7 +20,9 @@ const config = {
             case 1:
                 life.reset();
                 progress.reset();
+                progress.setTitle('Ajude o rei Arthur a atravessar a ponte, complete a ponte com uma forma geométrica');
                 home.show('Em busca da coroa', 'Jogar', './assets/images/home-arthur-and-merlin.png');
+                this.changeBackgroundImage('./assets/images/bridge-wallpaper.png');
                 cutscene
                     .show(
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
