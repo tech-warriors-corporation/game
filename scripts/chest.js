420661241de2759e36ea0268bcca547d5d1fb2c5
hotequil <joaohotequil@gmail.com>
2021-09-05 16:14:45 -0300

    Iniciada logica do chest.

--- scripts/chest.js
+++ scripts/chest.js
@@ -1,6 +1,9 @@
 const chest = {
     element: document.querySelector('[data-chest]'),
+    key: document.querySelector('[data-chest-key]'),
     hideClass: 'chest--hide',
+    showKeyClass: 'chest__key--show',
+    foundKey: false,
     hide: function(){
         this.element.classList.add(this.hideClass);
     },
@@ -9,8 +12,19 @@ const chest = {
     },
     endAssets: function(){
         this.hide();
+        this.resetKey();
+    },
+    resetKey: function(){
+        this.foundKey = false;
+        this.key.classList.remove(this.showKeyClass)
+    },
+    upKey: function(){
+        this.foundKey = true;
+        this.key.classList.add(this.showKeyClass)
     },
     init: function(){
         this.show();
+        this.resetKey();
+        this.key.onclick = () => !this.foundKey ? this.upKey() : null;
     }
 };
